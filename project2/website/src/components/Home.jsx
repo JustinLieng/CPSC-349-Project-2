4be4c40e3363a957f220da07a7a92346509058c7
Justin Lieng <70175007+JustinLieng@users.noreply.github.com>
2024-05-13 21:26:42 -0700

    Added 3D Models, GIFs, and CSS

--- project2/website/src/components/Home.jsx
+++ project2/website/src/components/Home.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Home = () => {
   return (
+    <div>
+        <div>
+        <img class="speechbubble" src="./src/pictures/speech bubble.gif" width="700"></img>
+        </div>
+        <iframe class="iframe" src="./src/3D Models/Banana Cat/" width="10%"></iframe>
+        <iframe class="iframe" src="./src/3D Models/Cat Box/" width="10%"></iframe>
+        <iframe class="iframe" src="./src/3D Models/Cat Box 2/" width="10%"></iframe>
     <div class="home">
       <h2>Welcome to Justin's portfolio website!</h2>
-      <p>In this portfolio website, I will showcase my software developer skills, projects, and much more.</p>
-      <p>This is a quick little picture of myself! Head down below for a full introduction.</p>
+      <p>In this portfolio website, I will showcase my software developer skills, projects, and much more. 
+        This is a quick little picture of myself! Head down below for a full introduction.</p>
       <img src="./src/pictures/nike.jpg" alt="Picture of Justin" width="400"></img>
       <h1>Hey, my name is Justin Lieng...</h1>
       <p>I am currently 22 years old and studying Computer Science at California State University Fullerton. 
@@ -15,6 +22,11 @@ const Home = () => {
         at all. I have no regrets going into this field not just because I enjoy it but becayse I don't see myself doing
         anything else. I hope you enjoy my portfolio website that I've created, please take a look around and come again!
       </p>
+      <h1>Skills</h1>
+      <p>Some of my programming related skill include: </p>
+      <h2>Languages</h2>
+      <h2>Developer Workflow</h2>
+    </div>
     </div>
   );
 };
