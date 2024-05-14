671096f5112dd6af9a285d3690de596362e94d0a
Justin Lieng <70175007+JustinLieng@users.noreply.github.com>
2024-05-13 20:24:37 -0700

    Added an introduction

--- project2/website/src/components/Home.jsx
+++ project2/website/src/components/Home.jsx
@@ -3,9 +3,18 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
-      <p>Welcome to Justin's personal website!</p>
-      {/* <img src = pictures/nike photo></img> */}
+      <h2>Welcome to Justin's portfolio website!</h2>
+      <p>In this portfolio website, I will showcase my software developer skills, projects, and much more.</p>
+      <p>This is a quick little picture of myself! Head down below for a full introduction.</p>
+      <img src="./src/pictures/nike.jpg" alt="Picture of Justin" width="400"></img>
+      <h1>Hey, my name is Justin Lieng...</h1>
+      <p>I am currently 22 years old and studying Computer Science at California State University Fullerton. 
+        This is my 4th year at CSUF, so I will be graduating after Spring Semester of 2024.
+        I decided to major in Computer Science because ever since I was a young child, I was always around computers.
+        I just gravitated towards technology in general and when the time came to deciding a major, I was not hesitant
+        at all. I have no regrets going into this field not just because I enjoy it but becayse I don't see myself doing
+        anything else. I hope you enjoy my portfolio website that I've created, please take a look around and come again!
+      </p>
     </div>
   );
 };
